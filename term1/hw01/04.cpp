754def0c3401bb9e5893c7d107541dd5ed41f1d5
Eugene <eugene.auduchinok@gmail.com>
2013-09-10 23:31:26 +0400

    Homework added.
    1.01.04, 1.01.07.

---
+++ term1/hw01/04.cpp
@@ -0,0 +1,31 @@
+/*
+Homework 1.01.04
+Lucky tickets
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum[28] = {0};
+
+	for (int i = 0; i <= 9; i++)
+		for (int j = 0; j <= 9; j++)
+			for (int k = 0; k <= 9; k++)
+			{
+				sum[i + j + k]++;
+			}
+
+	long long int answer = 0;
+
+	for (int i = 0; i <= 27; i++)
+	{
+		if (sum[i] == 1)
+			answer++;
+		else
+			answer += sum[i] * (sum[i] - 1);
+	}
+
+	cout << answer << endl;
+}
---
+++ term1/hw01/07.cpp
@@ -0,0 +1,40 @@
+/*
+Homework 1.01.05
+Prime nums
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+
+	cout << "n: ";
+	cin >> n;
+
+	bool *nums = new bool[n + 1];
+
+	for (int i = 0; i <= n; i++)
+		nums[i] = true;
+
+	nums[0] = false;
+	nums[1] = false;
+
+	for (int i = 2; i <= n; i++)
+	{
+		if (!nums[i])
+			continue;
+
+		for (int j = i * 2; j <= n; j += i)
+		{
+			nums[j] = false;
+		}
+
+		cout << i << " ";
+	}
+
+	cout << endl;
+
+	delete[] nums;
+}
