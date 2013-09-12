9342996a775b0c26290fb483f08e793fd0485f45
Eugene <eugene.auduchinok@gmail.com>
2013-09-12 11:47:33 +0400

    Homework added.
    1.01.09.

---
+++ term1/hw01/09.cpp
@@ -0,0 +1,52 @@
+/* 
+Homework 1.01.09
+Power function
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double pow(int a, int n)
+{
+	if (a == 0)
+	{
+		return 0;
+	}
+
+	double answer = 1;
+
+	if (n == 0)
+		return answer;
+
+	for (int i = 0; i < abs(n); i++)
+	{
+		if (n > 0)
+			answer *= a;
+		else
+			answer /= a;
+	}
+
+	return answer;
+}
+
+int main()
+{
+	double a = 0;
+
+	cout << "a: ";
+	cin >> a;
+
+	int n = 0;
+
+	cout << "n: ";
+	cin >> n;
+
+	if (a == 0 && n < 0)
+	{
+		cout << "Exception: division by 0" << endl;	
+		return 1;
+	}
+
+	cout << pow(a, n) << endl;
+}
