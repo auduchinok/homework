b049302105333e529c1e89f48f5848bc7de6778d
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-04-04 15:12:22 +0400

    Add 171-test1 Huffman

---
+++ x71/term2/test1/Huffman.fs
@@ -0,0 +1,32 @@
+﻿//module Huffman
+
+type CodeTree =
+  | Fork of CodeTree * CodeTree * char list * int
+  | Leaf of char * int
+
+
+// code tree
+
+let createCodeTree (chars: char list) : CodeTree = 
+    failwith "Not implemented"
+
+// decode
+
+type Bit = int 
+
+let decode (tree: CodeTree)  (bits: Bit list) : char list = 
+    let rec decode' (node: CodeTree) (bits: Bit list) =
+        match node with
+        | Fork(l, r, _, _) ->
+            match bits.Head with
+            | 0 -> decode' l bits.Tail
+            | 1 -> decode' r bits.Tail
+            | _ -> failwith "Wrong symbol"
+        | Leaf(c, _) ->
+            if bits.IsEmpty then c :: []
+            else c :: decode' tree bits
+    decode' tree bits |> List.rev
+// encode
+
+let encode (tree: CodeTree)  (text: char list) : Bit list = 
+  failwith "Not implemented"
