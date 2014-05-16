30a8b087eeab6459d3ad3c64cf5f3870004caac9
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-05-16 18:57:41 +0400

    Add 171 Map'CPS

---
+++ x71/term2/hw03/Map'CPS/Map'CPS/Program.fs
@@ -0,0 +1,27 @@
+﻿// Eugene Auduchinok (2014)
+
+// 'a list -> ('a -> 'b) -> ('b list -> 'c) -> 'c
+let map'cps l f k =
+    let rec map' processed left =
+        match left with
+        | [] -> k processed
+        | head :: tail -> map' (processed @ [f head]) tail
+    
+    map' [] l
+
+map'cps [1;2;3] (fun x -> x + 1) (printfn "%A")
+
+
+let isEven x k = k (x % 2 = 0)
+
+// 'a list -> ('a -> ('b -> 'c) -> 'c) -> ('b list -> 'c) -> 'c
+let rec map l f k =
+    match l with
+    | [] -> k []
+    | head :: tail ->
+        f head (fun head' -> map tail f (fun tail' -> k (head' :: tail')))
+
+
+let list = [1..10]
+
+list |> (fun x -> map x isEven (fun x -> printfn "%A" x))
--- x71/term2/hw03/mapcps.fs
+++
@@ -1,9 +0,0 @@
-﻿let map'cps l f k =
-    let rec map' processed left =
-        match left with
-        | [] -> k processed
-        | head :: tail -> map' (processed @ [f head]) tail
-    
-    map' [] l
-
-map'cps [1;2;3] (fun x -> x + 1) (printfn "%A")
