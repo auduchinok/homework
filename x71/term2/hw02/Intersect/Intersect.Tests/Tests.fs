7ec234e3253d0d1205ead6aab73610af52978e84
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-05-30 06:26:01 +0400

    Finish 171-Intersect

--- x71/term2/hw02/Intersect/Intersect.Tests/Tests.fs
+++ x71/term2/hw02/Intersect/Intersect.Tests/Tests.fs
@@ -3,89 +3,89 @@
 open NUnit.Framework
 open FsUnit
 
-[<TestFixture>] 
-type ``Intersecting tests`` () =
+[<Test>]
+let ``Segment to line`` () =
+    Intersect.segmentToLine (LineSegment((2.0, 1.0), (1.0, 3.0)))
+    |> should (equalWithin 0.5) (Line(-2.0, 5.0))
 
-    [<Test>]
-    let ``Segment to line`` () =
-        Intersect.segmentToLine (LineSegment((2.0, 1.0), (1.0, 3.0)))
-        |> should equal (Line(-2.0, 5.0))
+[<Test>]
+let ``Equal points`` () = 
+    intersect (Intersect (Point (1.0, 2.0), Point(1.0, 2.0)))
+    |> should equal (Point (1.0, 2.0))
 
-    [<Test>]
-    let ``Equal points`` () = 
-        intersect (Intersect (Point (1.0, 2.0), Point(1.0, 2.0)))
-        |> should equal (Point (1.0, 2.0))
+[<Test>]
+let ``Equal lines`` () =
+    intersect (Intersect (Line(1.0, 0.0), Line(1.0, 0.0)))
+    |> should equal (Line(1.0, 0.0))
 
-    [<Test>]
-    let ``Equal lines`` () =
-        intersect (Intersect (Line(1.0, 0.0), Line(1.0, 0.0)))
-        |> should equal (Line(1.0, 0.0))
+[<Test>]
+let ``Point and line`` () =
+    intersect (Intersect (Line(1.0, 0.0), Point(2.0, 2.0)))
+    |> should equal (Point(2.0, 2.0))
 
-    [<Test>]
-    let ``Point and line`` () =
-        intersect (Intersect (Line(1.0, 0.0), Point(2.0, 2.0)))
-        |> should equal (Point(2.0, 2.0))
+[<Test>]
+let ``Line and vertical Line`` () =
+    intersect (Intersect (Line (1.0, 2.0), VerticalLine (4.0)))
+    |> should equal (Point(4.0, 6.0))
 
-    [<Test>]
-    let ``Line and vertical Line`` () =
-        intersect (Intersect (Line (1.0, 2.0), VerticalLine (4.0)))
-        |> should equal (Point(4.0, 6.0))
+[<Test>]
+let ``Point and line segment`` () =
+    intersect (Intersect (LineSegment ((1.0, 1.0), (3.0, 2.0)), Point (2.0, 1.5)))
+    |> should equal (Point (2.0, 1.5))
 
-    [<Test>]
-    let ``Point and line segment`` () =
-        intersect (Intersect (LineSegment ((1.0, 1.0), (3.0, 2.0)), Point (2.0, 1.5)))
-        |> should equal (Point (2.0, 1.5))
+[<Test>]
+let ``Line and line segment`` () =
+        intersect (Intersect (Line (2.0, -1.0), LineSegment ((1.0, 3.0), (2.0, 1.0))))
+        |> should equal (Point (1.5, 2.0))
 
-    [<Test>]
-    let ``Line and line segment`` () =
-         intersect (Intersect (Line (2.0, -1.0), LineSegment ((1.0, 3.0), (2.0, 1.0))))
-         |> should equal (Point (1.5, 2.0))
-
-    [<Test>]
-    let ``Vertical lines`` () =
-        intersect (Intersect (VerticalLine (1.0), VerticalLine (1.0)))
-        |> should equal (VerticalLine (1.0))
+[<Test>]
+let ``Vertical lines`` () =
+    intersect (Intersect (VerticalLine (1.0), VerticalLine (1.0)))
+    |> should equal (VerticalLine (1.0))
     
-    [<Test>]
-    let ``Two lines and point`` () =
-        intersect (Intersect (Intersect (Line (1.0, 1.0), Line (-1.0, 5.0)), Point (2.0, 3.0)))
-        |> should equal (Point (2.0, 3.0))
+[<Test>]
+let ``Two lines and point`` () =
+    intersect (Intersect (Intersect (Line (1.0, 1.0), Line (-1.0, 5.0)), Point (2.0, 3.0)))
+    |> should equal (Point (2.0, 3.0))
 
-    [<Test>]
-    let ``Line segment and vertical line`` () =
-        intersect (Intersect (LineSegment ((1.0, 2.0), (1.0, 5.0)), VerticalLine (1.0)))
-        |> should equal (LineSegment ((1.0, 2.0), (1.0, 5.0)))
+[<Test>]
+let ``Line segment and vertical line`` () =
+    intersect (Intersect (LineSegment ((1.0, 2.0), (1.0, 5.0)), VerticalLine (1.0)))
+    |> should equal (LineSegment ((1.0, 2.0), (1.0, 5.0)))
 
 
-[<TestFixture>]
-type ``Non-intersecting tests`` () =
+
+[<Test>]
+let ``Two points`` () =
+    intersect (Intersect (Point (1.0, 2.0), Point (1.1, 2.2)))
+    |> should equal (NoPoint)
     
-    [<Test>]
-    let ``Two points`` () =
-        intersect (Intersect (Point (1.0, 2.0), Point (1.1, 2.2)))
-        |> should equal (NoPoint)
+[<Test>]
+let ``No point and line`` () =
+    intersect (Intersect (NoPoint, Line(1.0, 2.0)))
+    |> should equal (NoPoint)
+
+[<Test>]
+let ``Parallel lines`` () =
+    intersect (Intersect (Line(1.0, 0.0), Line(1.0, 1.0)))
+    |> should equal (NoPoint)
+
+[<Test>]
+let ``Non-intersecting line segment and verical line`` () =
+    intersect (Intersect (VerticalLine (-1.0), LineSegment ((-3.0, 1.0), (-2.0, 3.0))))
+    |> should equal (NoPoint)
+
+[<Test>]
+let ``(non-intersecting) Point and line segment`` () = 
+    intersect (Intersect (LineSegment ((-3.0, 1.0), (-2.0, 3.0)), Point (-1.0, 5.0)))
+    |> should equal (NoPoint)
     
-    [<Test>]
-    let ``No point and line`` () =
-        intersect (Intersect (NoPoint, Line(1.0, 2.0)))
-        |> should equal (NoPoint)
+[<Test>]
+let ``(non-intersecting) Vertical lines`` () =
+    intersect (Intersect (VerticalLine (1.0), VerticalLine (2.0)))
+    |> should equal (NoPoint)
 
-    [<Test>]
-    let ``Parallel lines`` () =
-        intersect (Intersect (Line(1.0, 0.0), Line(1.0, 1.0)))
-        |> should equal (NoPoint)
-
-    [<Test>]
-    let ``Non-intersecting line segment and verical line`` () =
-        intersect (Intersect (VerticalLine (-1.0), LineSegment ((-3.0, 1.0), (-2.0, 3.0))))
-        |> should equal (NoPoint)
-
-    [<Test>]
-    let ``Point and line segment`` () = 
-        intersect (Intersect (LineSegment ((-3.0, 1.0), (-2.0, 3.0)), Point (-1.0, 5.0)))
-        |> should equal (NoPoint)
-    
-    [<Test>]
-    let ``Vertical lines`` () =
-        intersect (Intersect (VerticalLine (1.0), VerticalLine (2.0)))
-        |> should equal (NoPoint)
+[<Test>]
+let ``Intersecting segments `` () =
+    intersect (Intersect (LineSegment ((1.0, 1.0), (3.0, 3.0)), LineSegment ((2.0, 2.0), (5.0, 5.0))))
+    |> should equal (LineSegment ((3.0, 3.0), (2.0, 2.0)))
--- x71/term2/hw02/Intersect/Intersect/Program.fs
+++ x71/term2/hw02/Intersect/Intersect/Program.fs
@@ -1,7 +1,12 @@
 ﻿// Eugene Auduchinok (c) 2014
 module Intersect
 
+let (?=) (a: float) (b: float) = 
+    let eps = 1.e-10
+    abs (a - b) < eps
+
 // It's important to remember that Line and VerticalLine are different types here.
+[<CustomEquality; NoComparison>]
 type Set =
     | NoPoint
     | Point of float * float
@@ -9,13 +14,20 @@ type Set =
     | VerticalLine of float
     | LineSegment of (float * float) * (float * float)
     | Intersect of Set * Set
-
-
-// заменить
-let (=) (a:float) (b:float) = 
-    let eps = 1.e-10
-    abs (a - b) < eps
-
+    
+    override x.Equals (o: obj) =
+        match o with
+        | :? Set as o ->
+            match x, o with
+            | NoPoint, NoPoint -> true
+            | Point (x1, y1), Point (x2, y2) -> x1 ?= x2 && y1 ?= y2
+            | Line (a1, b1), Line (a2, b2) -> a1 ?= a2 && b1 ?= b2
+            | VerticalLine (x1), VerticalLine (x2) -> x1 ?= x2
+            | LineSegment ((x1, y1), (x2, y2)), LineSegment ((x3, y3), (x4, y4)) ->
+                (x1 ?= x3 && y1 ?= y3 && x2 ?= x4 && y2 ?= y4) || (x1 ?= x4 && y1 ?= y4 && x2 ?= x3 && y2 ?= y3)
+            | _ -> false
+        | _ -> false
+    override x.GetHashCode() = hash x
    
 let segmentToLine = function
     | LineSegment((x1, y1), (x2, y2)) ->
@@ -42,36 +54,36 @@ let rec intersect = function
 
         // Two points.
         | Point (x1, y1), Point (x2, y2) ->
-            if x1 = x2 && y1 = y2 then Point (x1, y1)
+            if x1 ?= x2 && y1 ?= y2 then Point (x1, y1)
             else NoPoint
         
         // Point and vertical line.
         | Point (x0, y0), VerticalLine (x)
         | VerticalLine (x), Point (x0, y0) ->
-            if x0 = x then Point (x0, y0) else NoPoint
+            if x0 ?= x then Point (x0, y0) else NoPoint
 
         // Point and line.
         | Point (x, y), Line (a, b)
         | Line (a, b), Point (x, y) ->
-            if a * x + b = y then Point (x, y) else NoPoint
+            if a * x + b ?= y then Point (x, y) else NoPoint
 
         // Point and line segment.
         | Point (x0, y0), LineSegment ((x1, y1), (x2, y2))
         | LineSegment ((x1, y1), (x2, y2)), Point (x0, y0) ->
-            if x1 = x2 then
-                if x0 = x1 && y0 >= min y1 y2 && y0 <= max y1 y2
+            if x1 ?= x2 then
+                if x0 ?= x1 && y0 >= min y1 y2 && y0 <= max y1 y2
                 then Point (x0, y0)
                 else NoPoint
             else
                 let p = (x0 - x2) / (x1 - x2)
-                if (x1 - x2) * (y0 - y2) = (x0 - x2) * (y1 - y2) && abs p <= 1.0
+                if (x1 - x2) * (y0 - y2) ?= (x0 - x2) * (y1 - y2) && p >= 0.0 && p <= 1.0
                 then Point (x0, y0)
                 else NoPoint
 
         // Two lines.
         | Line(a1, b1) as line, Line (a2, b2) ->
-            if a1 = a2 then
-                if b1 = b2 then line
+            if a1 ?= a2 then
+                if b1 ?= b2 then line
                 else NoPoint
             else
                 let x = (b1 - b2) / (a2 - a1)
@@ -98,7 +110,7 @@ let rec intersect = function
 
         // Two vertical lines.
         | VerticalLine (x1) as line, VerticalLine (x2) ->
-            if x1 = x2 then line else NoPoint
+            if x1 ?= x2 then line else NoPoint
 
         // Line segment and verical line.
         | VerticalLine (x), LineSegment ((x1, y1), (x2, y2))
@@ -110,14 +122,27 @@ let rec intersect = function
             | Point (_) -> intersect (Intersect (segment, linesIntersection))
             | _ -> failwith "Wrong type."
 
+        // Two Line Segments
         | LineSegment ((x1, y1), (x2, y2)), LineSegment ((x3, y3), (x4, y4)) ->
-            failwith "not implemented"
-            // приводим оба отрезка к прямым,
-            // смотрим их пересечение, если пересечение - точка, смотрим, принадлежит
-            // ли она исходным отрезкам, если - прямая, нужно как-то найти границы общей части
+            let seg1 = a
+            let seg2 = b
+            let line1 = segmentToLine seg1
+            let line2 = segmentToLine seg2
+
+            match intersect (Intersect (line1, line2)) with
+            | NoPoint -> NoPoint
+            | Point (_) as point->
+                match intersect (Intersect (Intersect (point, seg1), (Intersect (point, seg2)))) with
+                | Point (_) -> point
+                | _ -> NoPoint
+            | Line (_) ->
+                let sortedPoints = Array.sortBy fst [|(x1, y1); (x2, y2); (x3, y3); (x4, y4)|]
+                LineSegment ((sortedPoints.[1], sortedPoints.[2]))
+
+            | _ -> failwith "Wrong type"
 
     | LineSegment ((x1, y1), (x2, y2)) as segment ->
-        if x1 = x2 && y1 = y2 then Point (x1, y1)
+        if x1 ?= x2 && y1 ?= y2 then Point (x1, y1)
         else segment
 
     | x -> x
