57101df3025a785708c6234d8835a9835e405c32
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-10-23 10:15:40 +0400

    Add email verifier task

---
+++ x71/term3/FSharp/EmailVerifier/EmailVerifier.Core/Library1.fs
@@ -0,0 +1,14 @@
+﻿namespace EmailVerifier.Core
+
+open System.Text.RegularExpressions
+
+module Verifier =
+
+    let localPart = "[a-z_](\.?\w+)*"
+    let subdomains = "([a-z]+\.)+"
+    let topLevelDomain = "(name|info|museum|[a-z]{2,3})"
+
+    let addressRegExp = "^" + localPart + "@" + subdomains + topLevelDomain + "$"
+
+    let regex = new Regex(addressRegExp, RegexOptions.IgnoreCase)
+    let IsValid address = regex.IsMatch(address)
---
+++ x71/term3/FSharp/EmailVerifier/EmailVerifier.Tests/Library1.fs
@@ -0,0 +1,52 @@
+﻿module EmailVerifier.Tests.Verifier
+
+open EmailVerifier.Core.Verifier
+open NUnit.Framework
+open FsUnit
+
+
+// Valid emails
+
+[<Test>]
+let ``Uppercase`` () = IsValid "ME@EXAMPLE.COM" |> should equal true
+
+[<Test>]
+let ``Single letter localpart & subdomain`` () = IsValid "a@b.cc" |> should equal true
+
+[<Test>]
+let ``Full name with a dot`` () = IsValid "victor.polozov@mail.ru" |> should equal true
+
+[<Test>]
+let ``'info' tld`` () = IsValid "my@domain.info" |> should equal true
+
+[<Test>]
+let ``Begins with underscore`` () = IsValid "_.1@mail.com" |> should equal true
+
+[<Test>]
+let ``Long tld`` () = IsValid "paints_department@hermitage.museum" |> should equal true
+
+
+// Invalid emails
+
+[<Test>]
+let ``Without at-symbol`` () = IsValid "Me.Com" |> should equal false
+
+[<Test>]
+let ``Single letter tld`` () = IsValid "a@b.c" |> should equal false
+
+[<Test>]
+let ``Two dotes in a row`` () = IsValid "a..b@mail.ru" |> should equal false
+
+[<Test>]
+let ``A dot in the beginning`` () = IsValid ".a@mail.ru" |> should equal false
+
+[<Test>]
+let ``A strange tld`` () = IsValid "yo@domain.somedomain" |> should equal false
+
+[<Test>]
+let ``Begins with a number`` () = IsValid "1@mail.ru" |> should equal false
+
+
+
+
+
