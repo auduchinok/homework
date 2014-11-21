f93f6e801ff06184f0d119fa3ee7a8e2dc693c89
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-11-21 03:27:23 +0300

    Add PartlyCloudy test work

---
+++ x71/term3/FSharp/PartlyCloudy/CloudTests/Tests.fs
@@ -0,0 +1,108 @@
+﻿module PartlyCloudy.Tests
+
+open NUnit.Framework
+open FsCheck
+open NSubstitute
+open PartlyCloudy.Core
+
+let test (dayLightArg : DaylightType, isShiningArg : bool, speedArg : int) (creatureType : CreatureType) =
+    let daylight = Substitute.For<IDaylight>()
+    let luminary = Substitute.For<ILuminary>()
+    let wind = Substitute.For<IWind>()
+
+    let magic = Substitute.For<IMagic>()
+    let courier = Substitute.For<ICourier>()
+
+    luminary.IsShining.Returns(isShiningArg) |> ignore
+    wind.Speed.Returns(speedArg)             |> ignore
+    daylight.Current.Returns(dayLightArg)    |> ignore
+    magic.CallDaemon().Returns(courier)      |> ignore
+    magic.CallStork().Returns(courier)       |> ignore
+
+    let cloud = new Cloud(daylight, luminary, wind, magic)
+    cloud.Create()
+
+    courier.Received().GiveBaby(Arg.Is<Creature> (fun (x : Creature) -> x.CreatureType = creatureType))
+
+
+
+[<Test>]
+let ``Balloon`` () = test (Morning, true, 0) Balloon 
+
+
+
+type KittenGen =
+    static member DaylightType() = Arb.fromGen <| Gen.elements [Morning; Day; Evening]
+    static member bool() = Arb.fromGen <| Gen.constant true
+    static member int() = Arb.fromGen <| Gen.choose (1,3)
+
+[<Test>]
+let ``Kitten`` () = 
+    Arb.register<KittenGen>() |> ignore
+    FsCheck.Check.QuickThrowOnFailure (fun x -> test x Kitten )
+
+
+
+type PuppyGen = 
+    static member DaylightType() = Arb.fromGen <| Gen.elements [Morning; Day; Evening]
+    static member bool() = Arb.fromGen <| Gen.constant true
+    static member int() = Arb.fromGen <| Gen.choose (4,6)
+
+[<Test>]
+let ``Puppy`` () =
+    Arb.register<PuppyGen>() |> ignore
+    FsCheck.Check.QuickThrowOnFailure (fun x -> test x Puppy )
+
+
+
+type HedgehogGen =
+    static member DaylightType() = Arb.fromGen <| Gen.elements [Morning; Day; Evening]
+    static member bool() = Arb.fromGen <| Gen.constant true
+    static member int() = Arb.fromGen <| Gen.choose (7, 10)
+
+[<Test>]
+let ``Hedgehog`` () =
+    Arb.register<HedgehogGen>() |> ignore
+    FsCheck.Check.QuickThrowOnFailure (fun x -> test x Hedgehog)
+
+
+
+type BearcubGen = 
+    static member DaylightType() = Arb.fromGen <| Gen.constant Night
+    static member bool() = Arb.fromGen <| Gen.constant true
+    static member int() = Arb.fromGen <| Gen.choose(0, 10)
+
+[<Test>]
+let ``Bearcub`` () = 
+    Arb.register<BearcubGen>() |> ignore
+    FsCheck.Check.QuickThrowOnFailure (fun x -> test x Bearcub)
+
+
+
+type PigletGen1 = 
+    static member DaylightType() = Arb.fromGen <| Gen.elements [Morning; Day; Evening]
+    static member bool() = Arb.fromGen <| Gen.constant false
+    static member int() = Arb.fromGen <| Gen.choose (0, 10)
+
+type PigletGen2 =
+    static member DaylightType() = Arb.fromGen <| Gen.constant Night
+    static member bool() = Arb.fromGen <| Gen.constant false
+    static member int() = Arb.fromGen <| Gen.choose (0, 9)
+
+[<Test>]
+let ``Piglet`` () =
+    Arb.register<PigletGen1>() |> ignore
+    Arb.register<PigletGen2>() |> ignore
+    FsCheck.Check.QuickThrowOnFailure (fun x -> test x Piglet)
+
+
+
+type BatGen = 
+    static member DaylightType() = Arb.fromGen <| Gen.constant Night
+    static member bool() = Arb.fromGen <| Gen.constant false
+    static member int() = Arb.fromGen <| Gen.constant 10
+
+[<Test>]
+let ``Bat`` () =
+    Arb.register<BatGen>() |> ignore
+    FsCheck.Check.QuickThrowOnFailure (fun x -> test x Bat)
---
+++ x71/term3/FSharp/PartlyCloudy/PartlyCloudy/Cloud.fs
@@ -0,0 +1,57 @@
+﻿module PartlyCloudy.Core
+
+type DaylightType = Morning | Day | Evening | Night
+
+type CreatureType =
+    | Puppy
+    | Kitten
+    | Hedgehog
+    | Bearcub
+    | Piglet
+    | Bat
+    | Balloon
+
+type Creature(creatureType : CreatureType) = 
+    member x.CreatureType = creatureType
+
+type IDaylight =
+    abstract member Current : DaylightType
+
+type ILuminary =
+    abstract member IsShining : bool
+
+type IWind =
+    abstract member Speed : int
+
+let speedConstraint = 11
+
+type ICourier =
+    abstract member GiveBaby : Creature -> unit
+
+type IMagic =
+    abstract member CallStork : unit -> ICourier
+    abstract member CallDaemon : unit -> ICourier
+
+type Cloud(daylight : IDaylight, luminary : ILuminary, wind : IWind, magic : IMagic) =
+    member private x.InternalCreate() =
+            match daylight.Current with
+            | Night ->
+                match luminary.IsShining with
+                | true -> Bearcub
+                | _ -> match wind.Speed with
+                       | 10 -> Bat
+                       | _ -> Piglet
+            | _ -> match luminary.IsShining with
+                   | false -> Piglet
+                   | _ -> match wind.Speed with
+                          | x when x > 0 && x <= 3 -> Kitten
+                          | x when x >= 4 && x <= 6 -> Puppy
+                          | x when x >= 7 -> Hedgehog
+                          | _ -> Balloon
+ 
+    member x.Create() =
+      let creature = new Creature(x.InternalCreate())
+      if creature.CreatureType = Bat then
+        magic.CallDaemon().GiveBaby(creature)
+      else
+        magic.CallStork().GiveBaby(creature)
