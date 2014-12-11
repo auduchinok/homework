b1d9fe6db162ae3dc6799bfa132852cbc75b2d05
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2014-12-12 00:22:24 +0300

    Add Bash builder task.

---
+++ x71/term3/Bash Builder/clone.sh
@@ -0,0 +1,29 @@
+# Eugene Auduchinok, 2014
+
+echo "Loading config.."
+source config.sh
+
+
+echo "Cleaning working folder.."
+rm -rf $REPO_NAME
+
+
+echo "Cloning project from remote repo.."
+git clone $REPO_PATH
+
+
+echo "Building workspace.."
+xcodebuild -workspace $WORKSPACE_PATH -scheme icreated -sdk iphonesimulator &> build_log.txt
+
+if grep -q "BUILD SUCCEEDED" build_log.txt
+then
+	printf "Build succeeded.\nSending email..\n"
+	mail -s "Build succeeded." $LOG_MAIL <<< "Yo. I've just finished building your project. It was OK."
+else
+	echo "There was an error during build."
+	echo "Sending email with log file.."
+	mutt -a build_log.txt -s "Build failed" -- $LOG_MAIL <<< "Build log is in the attachment."
+fi
+
+# I've used different ways to send email and to print text
+# for better remembering of using these things.
---
+++ x71/term3/Bash Builder/config.sh
@@ -0,0 +1,6 @@
+GITHUB_USER="eugene-auduchinok"
+REPO_NAME="icreated-ios"
+REPO_PATH="https://github.com/$GITHUB_USER/$REPO_NAME.git"
+APP_NAME="icreated"
+WORKSPACE_PATH="$REPO_NAME/$APP_NAME/$APP_NAME.xcworkspace"
+LOG_MAIL="eugene.auduchinok@gmail.com"
