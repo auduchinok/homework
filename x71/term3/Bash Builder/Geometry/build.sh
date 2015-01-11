a3ce668f88dcb4f9f92da37884adbb546c973297
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2015-01-11 19:39:58 +0300

    Modify builder

--- x71/term3/Bash Builder/Geometry/build.sh
+++ x71/term3/Bash Builder/Geometry/build.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 # Eugene Auduchinok, 2014
 
-echo "Loading config.."
-source config.sh
-
-
-echo "Cleaning working folder.."
-rm -rf $REPO_NAME
-
-
-echo "Cloning project from remote repo.."
-git clone $REPO_PATH
-
-
-echo "Building workspace.."
-xbuild $SOLUTION_PATH &> build_log.txt
-if find . -name "*.exe" | grep -q "bin"
-then
+function notify_success() {
 	printf "Build succeeded.\nSending email..\n"
-	mail -s "Build succeeded." $LOG_MAIL <<< "Yo. I've just finished building your project. It was OK."
-else
+	mutt -s "Build succeeded." -- $LOG_MAIL <<< "Yo. I've just finished building your project. It was OK."
+}
+
+function notify_fail() {
 	echo "There was an error during build."
 	echo "Sending email with log file.."
-	mutt -a build_log.txt -s "Build failed" -- $LOG_MAIL <<< "Build log is in the attachment."
-fi
+	mutt -a $1 -s "Build failed." -- $LOG_MAIL <<< "Build log is in the attachment."
+}
 
-# I've used different ways to send email and to print text
-# for better remembering of using these things.
+function clone() {
+	echo "Loading config.."
+	source config.sh
+
+	echo "Cleaning working folder.."
+	rm -rf $REPO_NAME
+
+	echo "Cloning project from remote repo.."
+	if ! git clone $REPO_PATH &> clone_log.txt
+	then
+	    notify_fail clone_log.txt
+	    exit 1
+	fi
+}
+
+function build() {
+	echo "Building workspace.."
+	xbuild $SOLUTION_PATH &> build_log.txt
+
+	while read file; do
+		if [ ! -f $file ]
+		then
+			notify_fail build_log.txt
+			exit 1
+		fi
+	done < expected_files.txt
+
+	notify_success
+}
+
+clone
+build
