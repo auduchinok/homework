98ab0454abee5da6af734c8437629bfcb876bc33
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2015-12-23 11:00:23 +0300

    Add update,view bd tasks

---
+++ x71/term5/db/Update.sql
@@ -0,0 +1,26 @@
+﻿-- 1. Insert Tyumen into cities table
+insert into cities (id, name) select max(id + 1), 'Tyumen' from cities
+
+-- 2. Cascade client deletion
+insert into clients (id, name, phone_number, address, info) values ((select max(id) + 1 from clients), 'Eugene', null, null, null)
+
+insert into orders (id, client_id, origin_city_id, destination_city_id, weight, cost)
+	values ((select max(id) + 1 from orders), (select id from clients where name = 'Eugene'), 4, 5, 49.36, 55.05)
+
+ alter table K4_orders drop constraint FK_K4_orders_client
+ alter table K4_orders add constraint FK_K4_orders_client foreign key (client_id) references K4_clients(id) on delete cascade
+
+ alter table K4_containers drop constraint FK_K4_containers_order
+ alter table K4_containers add constraint FK_K4_containers_order foreign key (order_id) references K4_orders(id) on delete cascade
+
+ delete clients where name = 'Eugene'
+
+-- 3. Add additional info to the first client
+update clients set info = 'Meow' where id = 1
+
+-- 4. Enlarge maximum ships load
+update ships set maximum_load = maximum_load * 1.1
+
+-- 5. Add additional info to clients used container number 031218
+update clients set info = 'Used 031218'
+	where id in (select client_id from orders join containers on containers.order_id = orders.id where number = 031218)
---
+++ x71/term5/db/View.sql
@@ -0,0 +1,24 @@
+﻿-- 1. Select loyal clients for promotion
+create view loyal_clients (name, phone)
+	as select name, phone_number from clients join orders on orders.client_id = clients.id
+	group by client_id, name, phone_number having count(*) > 1
+
+select * from loyal_clients
+
+drop view loyal_clients
+
+-- 2. Cancel heavy orders
+create view heavy_orders (id, weight)
+	as select id, weight from orders where weight > 75
+
+delete from heavy_orders
+
+-- 3. Decrease used containers types maximum load
+create view used_containers (id, number)
+	as select containers.id, number from containers
+	join orders on containers.order_id = orders.id
+
+update container_types set maximum_load = maximum_load - 3
+	where id in (select container_type_id from containers where id in (select id from used_containers))
+
+drop view used_containers
